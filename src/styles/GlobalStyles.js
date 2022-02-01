576c1a1f75a93d640812c88de6babc5eb1f84e0b
Walison <theodoro.walison@gmail.com>
2022-01-31 21:49:38 -0300

    feat: init

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  :root {
+    --black: #212121;
+    --white: #FFF;
+    --gray: #EEE;
+    --text-black: #151812;
+    --text-white: #EEE;
+    --primary: orange;
+    --secondary: #F80;
+    --shadow: #808080;
+
+    --pink: #FFADDD;
+    --blue: #82B2FB;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html, body #root {
+    height: 100%;
+  }
+  body {
+    font-family: 'Noto Sans JP', sans-serif;
+  }
+  @media(max-width: 1080px) {
+    html {
+        font-size: 93.75%;
+      }
+  }
+  @media(max-width: 768px) {
+    html {
+        font-size: 87.50%;
+    }
+  }
+`
