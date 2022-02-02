fa4e1f1cf760cd7cb20bd76bec7c09df6b2b2215
Walison <theodoro.walison@gmail.com>
2022-02-02 18:31:36 -0300

    fix: section min height

--- src/components/sections/Home/styles.js
+++ src/components/sections/Home/styles.js
@@ -2,7 +2,6 @@ import styled from 'styled-components'
 
 export const Section = styled.section`
   min-height: 100vh;
-  padding-top: 100px;
   display: flex;
   justify-content: center;
   align-items: center;
