8961091c7f200af1ae6f4946f755107444c5cf60
Walison <theodoro.walison@gmail.com>
2022-02-04 17:23:57 -0300

    test background gradient deploy

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -13,7 +13,7 @@ const Home = () => {
   return (
     <Container>
       <Header />
-
+      
       <HomeSection />
       <StorySection />
       <RoadmapSection />
--- src/components/pages/Home/styles.js
+++ src/components/pages/Home/styles.js
@@ -5,5 +5,14 @@ export const Container = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: linear-gradient(180deg, #B9EBF6 0%, #FFF8EC 16.15%, #EA90A0 41.67%, #69529A 70.83%, #7978B1 100%);
+  background: linear-gradient(
+    180deg,
+    #B9EBF6 0%,
+    #FFF8EC 6.15%,
+    #EA90A0 11.67%,
+    #69529A 25.83%,
+    #7978B1 50%,
+    #392A51 75%,
+    #1B1623 100%
+  );
 `
