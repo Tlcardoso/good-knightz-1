cb867e3dc6792d7b3c6ad2f5ab83e596cdf88422
Walison <theodoro.walison@gmail.com>
2022-02-05 17:08:15 -0300

    fix: partner section margin

--- src/components/sections/Partner/styles.js
+++ src/components/sections/Partner/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Section = styled.section`
   width: 100%;
   max-width: 1200px;
-  margin: 200px 0;
+  margin: 200px auto;
   display: flex;
   align-items: center;
   justify-content: space-around;  
