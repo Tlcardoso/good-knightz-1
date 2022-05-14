c1e6b22652ed472a5b0a9fea17436a217ef25fbe
Tlcardoso <tl.cardosogit@gmail.com>
2022-05-13 22:58:18 -0300

    Add secondary color props to faqCard component

--- src/components/cards/FaqCard/index.js
+++ src/components/cards/FaqCard/index.js
@@ -1,18 +1,30 @@
 import { useState } from "react"
 import { Container } from "./styles"
 
-const FaqCard = ({ question, answer }) => {
+const FaqCard = ({ question, answer, secondarycolor }) => {
   const [showAnswer, setShowAnswer] = useState(false)
 
   const toggleAnswer = () => {
     setShowAnswer(!showAnswer)
   }
 
+  const buttonColor = {bgColor: "9948E6", xColor: "FFF"}
+
   return (
     <Container isOpen={showAnswer}>
-      <div className="toggle" onClick={() => toggleAnswer()}>
-        <div className="vertical"/>
-        <div className="horizontal"/>
+      <div 
+      className="toggle" 
+      onClick={() => toggleAnswer()}
+      style={{ backgroundColor: `#${secondarycolor ? buttonColor.bgColor : null}` }}
+      >
+        <div 
+        className="vertical" 
+        style={{ backgroundColor: `#${secondarycolor ? buttonColor.xColor : null}` }}
+        />
+        <div 
+        className="horizontal"
+        style={{ backgroundColor: `#${secondarycolor ? buttonColor.xColor : null}` }}
+        />
       </div>
       <p className='question'>{question}</p>
       {
